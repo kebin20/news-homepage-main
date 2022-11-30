a8cbb50f3e3091ac3b72617fc3d36d28905e704e
kebin20 <ktanzyl@hotmail.com>
2022-11-30 11:52:14 +0900

    Began on the desktop layout

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
 import './style.css';
-
-function showMenu() {
-  var x = document.getElementById('nav-menu');
-  if (x.style.display === 'block') {
-    x.style.display = 'none';
-  } else {
-    x.style.display = 'block';
-  }
-}
