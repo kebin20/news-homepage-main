730fc6e3d767c87e3f50536c1a8a966f921bfb97
kebin20 <ktanzyl@hotmail.com>
2022-12-01 10:08:22 +0900

    Fully completed hamburger menu

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import './style.css';
 const get = element => document.getElementById(element);
 
 let open = get("menu-btn");
-let nav = get("nav");
+// let nav = get("nav");
 let exit = get("exit-btn");
 
 open.addEventListener('click', () => {
